8a6ffbca817d8168b89c34e8a6770928ff9cb094
Jesse Wolgamott <jesse@comalproductions.com>
2017-07-07 12:05:42 -0500

    Add query and searching

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,11 +3,25 @@ const router = express.Router();
 const Game = require("../models/Game")
 
 router.get('/', function(req, res){
-  Game.find()
+  const queryObject = {};
+  if (req.query.name){
+    // queryObject.name = req.query.name;
+    // Instead of ^ we want to search substring
+    // We'll do that with a "Regular Expression"
+    // "i" means case _in_sensitive
+    queryObject.name = new RegExp(req.query.name, 'i');
+  }
+  if (req.query.year){
+    queryObject.year = req.query.year;
+  }
+
+  Game.find(queryObject)
   .sort("year")
   .then( function(games){
     res.render("index", {
-      games: games
+      games: games,
+      searchName: req.query.name,
+      searchYear: req.query.year
     })
   })
 })
